c745f00d5ea025cf17342cb140142dac1833cce6
furi.kiri <68464853+lilianna040@users.noreply.github.com>
2020-09-30 00:01:06 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
+  if (array===undefined || array.length==0)
   return 0;
+  let min=array[0];                                             
+  for (let i=1; i<array.length; i++) {
+    if (array[i]<min)
+      min=array[i];
+    }
+  return min; 
 }
 
 exports.max = function max (array) {
+  if (array===undefined || array.length==0)
   return 0;
+  let max=array[0];                                             
+  for (let i=1; i<array.length; i++) {
+    if (array[i]>max)
+      max=array[i];
+  }
+  // array.map( i => i>max ? max=i : max=max );
+  return max; 
 }
 
 exports.avg = function avg (array) {
+  if (array===undefined || array.length==0)
   return 0;
+  return (array.reduce((a,b) => a+b))/array.length;
 }
