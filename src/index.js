efd59c6dd1e0616bd1a57e4687876c34b34ddfcb
kukushkinaIra <55156673+kukushkinaIra@users.noreply.github.com>
2020-10-02 22:31:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
+  let min;
+  if ( array===undefined || array.length==0)
   return 0;
+  for (let i=0; array.length; i++) {
+    if (array[i]<array[i+1])
+     min=array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
+  if ( array===undefined || array.length==0)
   return 0;
+  for (let i=0; array.length; i++) {
+    if (array[i]>array[i+1])
+     min=array[i];
+  }
+  return min;
 }
 
 exports.avg = function avg (array) {
+  if ( array===undefined || array.length==0)
   return 0;
+  let avg=array.reduce(function(a,b) {
+    return (a+b)/array.length;
+  })
+  return avg;
 }
