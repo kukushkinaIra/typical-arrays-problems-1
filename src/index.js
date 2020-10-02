55612fba07ed7a105f2b7fdbe00019eb2e205c92
kukushkinaIra <55156673+kukushkinaIra@users.noreply.github.com>
2020-10-02 23:17:39 +0300

    feat:fix solution

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
+
 exports.min = function min (array) {
-  if (array===undefined || array.length==0)
+  if ( array===undefined || array.length==0)
   return 0;
-  let min=array[0];                                             
+  let min=array[0];
   for (let i=1; i<array.length; i++) {
     if (array[i]<min)
-      min=array[i];
-    }
-  return min; 
+     min=array[i];
+  }
+  console.log(min);
+  return min;
 }
 
 exports.max = function max (array) {
-  if (array===undefined || array.length==0)
+  if ( array===undefined || array.length==0)
   return 0;
-  let max=array[0];                                             
+  let max=array[0];
   for (let i=1; i<array.length; i++) {
     if (array[i]>max)
-      max=array[i];
+     max=array[i];
   }
-  // array.map( i => i>max ? max=i : max=max );
-  return max; 
+  return max;
 }
 
 exports.avg = function avg (array) {
-  if (array===undefined || array.length==0)
+  if ( array===undefined || array.length==0)
   return 0;
-  return (array.reduce((a,b) => a+b))/array.length;
+  let avg=array.reduce(function (a,b) {
+    return a+b;
+  })
+  return avg/array.length;
 }
